42764739223706947c7df3196512ac7234a4b4ae
Matthew Jett <mjj0013@uah.edu>
2021-12-23 09:49:26 -0600

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,86 +1,161 @@
 import React from 'react';
-import {Container} from 'semantic-ui-react';
+import "./globalStyles.css"
+import {Button, Container, Menu} from 'semantic-ui-react';
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 class Home extends React.Component {
-
 	constructor(props) {
 		super(props);
         this.makeEpicycloid = this.makeEpicycloid.bind(this);
         this.editEpicycloid = this.editEpicycloid.bind(this);
         this.animateEpicycloid = this.animateEpicycloid.bind(this);
+        this.switchEpicycloid = this.switchEpicycloid.bind(this);
         this.cycloidPaths = [];
         this.svgWidth=800;
-        this.svgHeight=800;
-        
+        this.svgHeight=700;
+        this.currentEpicycloid = 1;
 	}
+
     componentDidMount() {
-
-
-        //***************************************** */
-        this.makeEpicycloid(1,1.5);
-        this.editEpicycloid(2,3, 45, 75, 1, 1);
-        this.editEpicycloid(4,6, 45, 75, 1, 1);
-        this.editEpicycloid(8,12, 45, 75, 1, 1);
-        //***************************************** */
-        // this.makeEpicycloid(1,5);
-        // this.editEpicycloid(2,10, 45, 75, 1, 1);
-        // this.editEpicycloid(4,20, 45, 75, 1, 1);
-        // this.editEpicycloid(8,40, 45, 75, 1, 1);
-        
-        //***************************************** */
-        // this.makeEpicycloid(.6,1.2);
-        // this.editEpicycloid(1.2,2.4, 45, 75, 1, 1);
-
-        //***************************************** */
-        // this.makeEpicycloid(1,3);
-        // this.editEpicycloid(2,6, 45, 75, 1, 1);
-        // this.editEpicycloid(4,12, 45, 75, 1, 1);
-
-        //***************************************** */
-        // this.makeEpicycloid(2,4);
-        // this.editEpicycloid(2,4, 135, 75, 1, 1);
-        // this.editEpicycloid(2,4, 180, 75, .5, .5);
-        // this.editEpicycloid(1,2, 30, 75, .5, .5);
-        // this.editEpicycloid(.5,1, 45, 75, 1, 1);
-        // this.editEpicycloid(.5,1, 90, 75, .5, .5);
-        // this.editEpicycloid(.25,.5, 225, 75, 1, 1);
-        // this.editEpicycloid(.25,.5, 270, 75, .5, .5);
-        // this.editEpicycloid(.125,.25, 225, 75, 1, 1);
-        // this.editEpicycloid(.125,.25, 270, 75, .5, .5);
-
-
-
-        //***************************************** */
-        // this.makeEpicycloid(3,6);
-        // this.editEpicycloid(3,6, 135, 75, 1, 1);
-        // this.editEpicycloid(3,6, 180, 75, .5, .5);
-        // this.editEpicycloid(1.5,3, 30, 75, .5, .5);
-        // this.editEpicycloid(1.5,3, 45, 75, 1, 1);
-        // this.editEpicycloid(.75,1.5, 90, 75, .5, .5);
-        // this.editEpicycloid(.375,.75, 45, 75, 1, 1);
-        // this.editEpicycloid(.1875,.375, 90, 75, .5, .5);
-        
+        this.makeEpicycloid(3,6);
+        this.editEpicycloid(3,6, 135, 75, 1, 1);
+        this.editEpicycloid(3,6, 180, 75, .5, .5);
+        this.editEpicycloid(1.5,3, 30, 75, .5, .5);
+        this.editEpicycloid(1.5,3, 45, 75, 1, 1);
+        this.editEpicycloid(.75,1.5, 90, 75, .5, .5);
+        this.editEpicycloid(.375,.75, 45, 75, 1, 1);
+        this.editEpicycloid(.1875,.375, 90, 75, .5, .5);
         this.animateEpicycloid();
-        
-       
+    }
+
+    switchEpicycloid(e) {
+        switch(e.target.id) {
+            case "sequenceButton1":
+                if(this.currentEpicycloid==1) break;
+                document.getElementById("epicycloidAnimation").remove();
+                document.getElementById("epicycloid").remove();
+                this.cycloidPaths =[];
+                document.getElementById("mainSVG").insertAdjacentHTML('beforeend',`<path id="epicycloid" stroke="black" fill="none"></path>`)
+                this.makeEpicycloid(3,6);
+                this.editEpicycloid(3,6, 135, 75, .5, .5);
+                this.editEpicycloid(3,6, 180, 75, .5, .5);
+                this.editEpicycloid(1.5,3, 30, 75, 1, 1);
+                this.editEpicycloid(1.5,3, 45, 75, 1, 1);
+                this.editEpicycloid(.75,1.5, 90, 75, .5, .5);
+                this.editEpicycloid(.75,1.5, 135, 75, 1, 1);
+                this.editEpicycloid(.375,.75, 180, 75, .5, .5);
+                this.editEpicycloid(.375,.75, 225, 75, 1, 1);
+                this.editEpicycloid(.1875,.375, 270, 75, .5, .5);
+                this.editEpicycloid(.1875,.375, 315, 75, 1, 1);
+                this.animateEpicycloid();
+                this.currentEpicycloid = 1;
+                break;
+
+            case "sequenceButton2":
+                if(this.currentEpicycloid==2) break;
+                document.getElementById("epicycloidAnimation").remove();
+                document.getElementById("epicycloid").remove();
+                this.cycloidPaths =[];
+                document.getElementById("mainSVG").insertAdjacentHTML('beforeend',`<path id="epicycloid" stroke="black" fill="none"></path>`)
+                this.makeEpicycloid(1,5);
+                this.editEpicycloid(1,5, 45, 75, .5, .5);
+                this.editEpicycloid(1,5, 90, 75, .5, .5);
+                this.editEpicycloid(2,10, 135, 75, 1, 1);
+                this.editEpicycloid(2,10, 180, 75, 1, 1);
+                this.editEpicycloid(4,20, 225, 75, .5, .5);
+                this.editEpicycloid(4,20, 270, 75, 1, 1);
+                this.editEpicycloid(8,40, 315, 75, .5, .5);
+                this.editEpicycloid(8,40, 360, 75, 1, 1);
+                this.animateEpicycloid();
+                this.currentEpicycloid = 2;
+                break;
+
+            case "sequenceButton3":
+                if(this.currentEpicycloid==3) break;
+                document.getElementById("epicycloidAnimation").remove();
+                document.getElementById("epicycloid").remove();
+                this.cycloidPaths =[];
+                document.getElementById("mainSVG").insertAdjacentHTML('beforeend',`<path id="epicycloid" stroke="black" fill="none"></path>`)
+                this.makeEpicycloid(.6,1.2);
+                this.editEpicycloid(.6,1.2, 45, 75, .5, .5);
+                this.editEpicycloid(.6,1.2, 90, 75, .5, .5);
+                this.editEpicycloid(1.2,2.4, 135, 75, 1, 1);
+                this.editEpicycloid(1.2,2.4, 180, 75, 1, 1);
+                this.animateEpicycloid();
+                this.currentEpicycloid = 3;
+                break;
+
+            case "sequenceButton4":
+                if(this.currentEpicycloid==4) break;
+                document.getElementById("epicycloidAnimation").remove();
+                document.getElementById("epicycloid").remove();
+                this.cycloidPaths =[];
+                document.getElementById("mainSVG").insertAdjacentHTML('beforeend',`<path id="epicycloid" stroke="black" fill="none"></path>`)
+                this.makeEpicycloid(1,3);
+                this.editEpicycloid(1,3, 45, 75, .5, .5);
+                this.editEpicycloid(1,3, 90, 75, .5, .5);
+                this.editEpicycloid(2,6, 135, 75, 1, 1);
+                this.editEpicycloid(2,6, 180, 75, 1, 1);
+                this.editEpicycloid(4,12, 225, 75, .5, .5);
+                this.editEpicycloid(4,12, 270, 75, .5, .5);
+                this.animateEpicycloid();
+                this.currentEpicycloid = 4;
+                break;
+
+            case "sequenceButton5":
+                if(this.currentEpicycloid==5) break;
+                document.getElementById("epicycloidAnimation").remove();
+                document.getElementById("epicycloid").remove();
+                this.cycloidPaths =[];
+                document.getElementById("mainSVG").insertAdjacentHTML('beforeend',`<path id="epicycloid" stroke="black" fill="none"></path>`)
+                this.makeEpicycloid(2,4);
+                this.editEpicycloid(2,4, 135, 75, 1, 1);
+                this.editEpicycloid(2,4, 180, 75, .5, .5);
+                this.editEpicycloid(1,2, 30, 75, .5, .5);
+                this.editEpicycloid(.5,1, 45, 75, 1, 1);
+                this.editEpicycloid(.5,1, 90, 75, .5, .5);
+                this.editEpicycloid(.25,.5, 225, 75, 1, 1);
+                this.editEpicycloid(.25,.5, 270, 75, .5, .5);
+                this.editEpicycloid(.125,.25, 225, 75, 1, 1);
+                this.editEpicycloid(.125,.25, 270, 75, .5, .5);
+                this.animateEpicycloid();
+                this.currentEpicycloid = 5;
+                break;
+
+            case "sequenceButton6":
+                if(this.currentEpicycloid==6) break;
+                document.getElementById("epicycloidAnimation").remove();
+                document.getElementById("epicycloid").remove();
+                this.cycloidPaths =[];
+                document.getElementById("mainSVG").insertAdjacentHTML('beforeend',`<path id="epicycloid" stroke="black" fill="none"></path>`)
+                this.makeEpicycloid(1,1.5);
+                this.editEpicycloid(2,3, 45, 75, 1, 1);
+                this.editEpicycloid(4,6, 45, 75, 1, 1);
+                this.editEpicycloid(8,12, 45, 75, 1, 1);
+                this.animateEpicycloid();
+                this.currentEpicycloid = 6;
+                break;
+        }
     }
     animateEpicycloid() {
         var pathString = ""
         var durPerPhase = 3;
+        var spline = ".5 1 0.75 1;"
+        var keySplines = ""
         var totalDur=this.cycloidPaths.length*durPerPhase;
         for(let i=0; i < this.cycloidPaths.length;++i) {
             pathString+=this.cycloidPaths[i]
             pathString+=";"
+            keySplines+=spline;
         }
-        document.getElementById("epicycloid").insertAdjacentHTML('beforeend',`<animate id="epicycloidAnimation" begin="0s" attributeName="d" attributeType="XML" values="${pathString}" dur="${totalDur}s" repeatCount="indefinite" />`);
+        document.getElementById("epicycloid").insertAdjacentHTML('beforeend',`<animate id="epicycloidAnimation"  begin="0s" attributeName="d" keySplines="${keySplines}" attributeType="XML" values="${pathString}" dur="${totalDur}s" repeatCount="indefinite" />`);
     }
     makeEpicycloid(p,q) {
         var k = p/q;
-        var r = 75;
+        var r = 100;
         var R = k*r;
         var imgCenter = {x:this.svgWidth/2-(R+r)/r, y:this.svgHeight/2-(R+r)/r}
         var radPerIter = 57.2958;
@@ -92,53 +167,38 @@ class Home extends React.Component {
         let x0 = ((R+r)*Math.cos(radPerIter*(a0)) - (r)*Math.cos(radPerIter*(a0)*(R+r)/r));
         let y0 = ((R+r)*Math.sin(radPerIter*(a0)) - (r)*Math.sin(radPerIter*(a0)*(R+r)/r));
         var d = `M${imgCenter.x+x0},${imgCenter.y+y0} `
-
         var numSegments = 0;
-
-        
-
-
         // var currentPathObj = document.createElementNS("http://www.w3.org/2000/svg", "path");
         // currentPathObj.setAttribute("id","path0");
         // currentPathObj.setAttribute("stroke",`hsl(${220}, ${getRandomInt(0,70)}%, ${getRandomInt(0,70)}%)`);
         // this.cycloidPaths.push([`M${imgCenter.x+x0},${imgCenter.y+y0} `])
         // var maxSegments = Math.floor(numCusps*360/degPerIter)
-        
         // var numSubPaths = Math.floor(maxSegments/3);
         // console.log("maxSegments",maxSegments)
         // for(let i =0; i<numCusps*360; i+=degPerIter) {
         //     if(i==0) continue;
         //     let pathOrder = Math.floor(i/degPerIter);
-            
         //     if(pathOrder%(maxSegments/numSubPaths)==0) {
         //         document.getElementById("epicycloidGroup").appendChild(currentPathObj);
-                
         //         var currentPathObj = document.createElementNS("http://www.w3.org/2000/svg", "path");
         //         currentPathObj.setAttribute("id",`path${this.cycloidPaths.length}`);
         //         currentPathObj.setAttribute("stroke",`hsl(${220}, ${getRandomInt(0,70)}%, ${getRandomInt(0,70)}%)`);
         //         currentPathObj.setAttribute("d",this.cycloidPaths[this.cycloidPaths.length-1][0]);
         //         currentPathObj.setAttribute("fill","none");
-
-
         //         let a = i;
         //         let x = ((R+r)*Math.cos(radPerIter*a) - (r)*Math.cos(radPerIter*(a)*(R+r)/r));
         //         let y = ((R+r)*Math.sin(radPerIter*a) - (r)*Math.sin(radPerIter*(a)*(R+r)/r));
         //         this.cycloidPaths.push([`M${imgCenter.x+x},${imgCenter.y+y} `]);
         //         this.cycloidPaths[this.cycloidPaths.length-1][0] +=`A${(R+r)/r} ${(R+r)/r} ${i} ${1} ${1} ${imgCenter.x+x},${imgCenter.y+y} `;
-                
-                
         //     }
         //     else {
         //         let a = i;
         //         let x = ((R+r)*Math.cos(radPerIter*a) - (r)*Math.cos(radPerIter*(a)*(R+r)/r));
         //         let y = ((R+r)*Math.sin(radPerIter*a) - (r)*Math.sin(radPerIter*(a)*(R+r)/r));
-
         //         // d+=`A${(R+r)/r} ${(R+r)/r} ${i} ${0} ${1} ${imgCenter.x+x},${imgCenter.y+y} `
         //         this.cycloidPaths[this.cycloidPaths.length-1][0] += `A${(R+r)/r} ${(R+r)/r} ${i} ${1} ${1} ${imgCenter.x+x},${imgCenter.y+y} `
         //     }
-            
         // }
-        console.log("this.cycloidPaths",this.cycloidPaths)
         // try:for(let i =0; i<(maxSegments)*(360); i+=degPerIter) {
         for(let i =0; i<(numCusps)*(360); i+=degPerIter) {
             if(i==0) continue;
@@ -148,8 +208,6 @@ class Home extends React.Component {
             d+=`A${(R+r)/r} ${(R+r)/r} ${i} ${0} ${1} ${imgCenter.x+x},${imgCenter.y+y} `
             ++numSegments;
         }
-      
-        console.log('numSegments', numSegments);
         var epicycloid = document.getElementById("epicycloid");
         epicycloid.setAttribute("d",d);
         this.cycloidPaths.push(d);
@@ -185,19 +243,21 @@ class Home extends React.Component {
     render() {
         return(
                 <Container id="homeContainer">
-                    <svg class="geoSVG" id="mainSVG" width={this.svgWidth} height={this.svgHeight} style={{border:"1px solid black"}}>
-                        <g id="epicycloidGroup">
-
-                        </g>
-                        <path id="epicycloid" stroke="black" fill="none">
-                            
-                        </path>
+                    <svg className="geoSVG" id="mainSVG" width={this.svgWidth} height={this.svgHeight}>
+                        <g id="epicycloidGroup"></g>
+                        <path id="epicycloid" fill="none" stroke="black" ></path>
                     </svg>
+                    <Menu id="sequenceSelector" >
+                        <Button id="sequenceButton1" className="item" onClick={(e)=>this.switchEpicycloid(e)}>Pattern 1</Button>
+                        <Button id="sequenceButton2" className="item" onClick={(e)=>this.switchEpicycloid(e)}>Pattern 2</Button>
+                        <Button id="sequenceButton3" className="item" onClick={(e)=>this.switchEpicycloid(e)}>Pattern 3</Button>
+                        <Button id="sequenceButton4" className="item" onClick={(e)=>this.switchEpicycloid(e)}>Pattern 4</Button>
+                        <Button id="sequenceButton5" className="item" onClick={(e)=>this.switchEpicycloid(e)}>Pattern 5</Button>
+                        <Button id="sequenceButton6" className="item" onClick={(e)=>this.switchEpicycloid(e)}>Pattern 6</Button>
+                    </Menu>
                 </Container>
-        )
+        );
     }
-
-    
 }
 
 export default Home;
